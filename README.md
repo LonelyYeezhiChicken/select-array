b7ed36df31a6d1e518f7a7ba175a6f29e7b01c16
chicken <hibary.taiwan@gmail.com>
2022-10-31 23:49:11 +0800

    docs: update readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # select-array
 練習oop包裝array新增移除
+
+### 使用方式
+- ` new  ArrayFilter(array) `
+- 初始化時傳入，需增減的陣列
+
+
+### 提供函式
+1. remove
+2. add
--- js/arrayFilter.js
+++ js/arrayFilter.js
@@ -3,6 +3,11 @@ class ArrayFilter {
     this._array = JSON.parse(JSON.stringify(array));
   }
 
+  /** 移除指定Id
+   *
+   * @param {string} id
+   * @returns 移除後的 array
+   */
   remove(id) {
     this._array = this._array.filter((v) => v != id);
     return this._array;
